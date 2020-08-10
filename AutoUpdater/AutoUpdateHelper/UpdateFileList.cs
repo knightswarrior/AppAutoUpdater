ed2c521ed2d9ee4dc12850b133768260eef6ca40
Victor Zeng <KnightsWarrior@msn.com>
2020-08-10 08:10:49 +0800

    Initial commit.

---
+++ AutoUpdater/AutoUpdateHelper/UpdateFileList.cs
@@ -0,0 +1,24 @@
+﻿/*****************************************************************
+ * Copyright (C) Knights Warrior Corporation. All rights reserved.
+ * 
+ * Author:   圣殿骑士（Knights Warrior） 
+ * Email:    KnightsWarrior@msn.com
+ * Website:  http://www.cnblogs.com/KnightsWarrior/       https://github.com/knightswarrior
+ * Create Date:  5/8/2010 
+ * Usage:
+ *
+ * RevisionHistory
+ * Date         Author               Description
+ * 
+*****************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnightsWarriorAutoupdater
+{
+    public class UpdateFileList : List<LocalFile>
+    {
+
+    }
+}
